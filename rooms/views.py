cf190ae96ca4fe989bcebcaff8fb71f7feba660b
zxxzyyz <52019293+zxxzyyz@users.noreply.github.com>
2020-01-14 21:57:45 +0900

    half done

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,199 @@
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
+from . import forms
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """ HomeView Views Definition """
+
+    model = models.Room
+    paginate_by = 10
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Views Definition """
+
+    model = models.Room
+
+
+class SearchView(View):
+
+    """ SearchView Deifinition """
+
+    def get(self, request):
+
+        country = request.GET.get("country")
+        if country:
+            form = forms.SearchForm(request.GET)
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__lte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__lte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__lte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__lte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = instant_book
+
+                if superhost is True:
+                    filter_args["host__superhost"] = superhost
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+                paginator = Paginator(qs, 10)
+                page = request.GET.get("page", 1)
+                rooms = paginator.get_page(page)
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
+
+        else:
+            form = forms.SearchForm()
+
+        return render(request, "rooms/search.html", {"form": form})
+
+
+# def search(request):
+
+#     country = request.GET.get("country")
+#     if country:
+#         form = forms.SearchForm(request.GET)
+#         if form.is_valid():
+#             city = form.cleaned_data.get("city")
+#             country = form.cleaned_data.get("country")
+#             room_type = form.cleaned_data.get("room_type")
+#             price = form.cleaned_data.get("price")
+#             guests = form.cleaned_data.get("guests")
+#             bedrooms = form.cleaned_data.get("bedrooms")
+#             beds = form.cleaned_data.get("beds")
+#             baths = form.cleaned_data.get("baths")
+#             instant_book = form.cleaned_data.get("instant_book")
+#             superhost = form.cleaned_data.get("superhost")
+#             amenities = form.cleaned_data.get("amenities")
+#             facilities = form.cleaned_data.get("facilities")
+
+#             filter_args = {}
+
+#             if city != "Anywhere":
+#                 filter_args["city__startswith"] = city
+
+#             filter_args["country"] = country
+
+#             if room_type is not None:
+#                 filter_args["room_type"] = room_type
+
+#             if price is not None:
+#                 filter_args["price__lte"] = price
+
+#             if guests is not None:
+#                 filter_args["guests__lte"] = guests
+
+#             if bedrooms is not None:
+#                 filter_args["bedrooms__lte"] = bedrooms
+
+#             if beds is not None:
+#                 filter_args["beds__lte"] = beds
+
+#             if baths is not None:
+#                 filter_args["baths__lte"] = baths
+
+#             if instant_book is True:
+#                 filter_args["instant_book"] = instant_book
+
+#             if superhost is True:
+#                 filter_args["host__superhost"] = superhost
+
+#             for amenity in amenities:
+#                 filter_args["amenities"] = amenity
+
+#             for facility in facilities:
+#                 filter_args["facilities"] = facility
+
+#             print(filter_args)
+#             rooms = models.Room.objects.filter(**filter_args)
+#             return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+
+#     else:
+#         form = forms.SearchForm()
+#         return render(request, "rooms/search.html", {"form": form})
+
+
+# form = {
+#     "city": city,
+#     "s_country": country,
+#     "s_room_type": room_type,
+#     "price": price,
+#     "guests": guests,
+#     "bedrooms": bedrooms,
+#     "beds": beds,
+#     "baths": baths,
+#     "s_amenities": s_amenities,
+#     "s_facilities": s_facilities,
+#     "instant": instant,
+#     "superhost": superhost,
+# }
+
+# room_types = models.RoomType.objects.all()
+# amenities = models.Amenity.objects.all()
+# facilities = models.Facility.objects.all()
+
+# choices = {
+#     "countries": countries,
+#     "room_types": room_types,
+#     "amenities": amenities,
+#     "facilities": facilities,
+# }
+
+# return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms},)
+
+
+# from django.shortcuts import render, redirect, reverse
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         return redirect(reverse("core:home"))
---
+++ users/forms.py
@@ -0,0 +1,83 @@
+from django import forms
+from . import models
+
+
+class LoginForm(forms.Form):
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user = super().save(commit=False)
+        user.username = email
+        user.set_password(password)
+        user.save()
+
+
+# class SignUpForm(forms.Form):
+
+#     first_name = forms.CharField(max_length=20)
+#     last_name = forms.CharField(max_length=20)
+#     email = forms.EmailField()
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+#     def clean_email(self):
+#         email = self.cleaned_data.get("email")
+#         try:
+#             models.User.objects.get(email=email)
+#             raise forms.ValidationError("User already exists with that email")
+#         except models.User.DoesNotExist:
+#             return email
+
+#     def clean_password1(self):
+#         password = self.cleaned_data.get("password")
+#         password1 = self.cleaned_data.get("password1")
+
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
+
+#     def save(self):
+#         first_name = self.cleaned_data.get("first_name")
+#         last_name = self.cleaned_data.get("last_name")
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+
+#         user = models.User.objects.create_user(email, email, password)
+#         user.first_name = first_name
+#         user.last_name = last_name
+#         user.save()
